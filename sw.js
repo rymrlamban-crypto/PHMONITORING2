8b8db7189096ed55059bd8b3efa20ca4e64847bd
rymrlamban-crypto <rymrlamban@gmail.com>
2026-04-10 15:23:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,30 @@
+// This allows the PWA to be installed
 self.addEventListener('fetch', (event) => {
-  // This is a basic service worker to allow PWA installation
+  // Basic fetch listener
+});
+
+// This listener allows the app to show notifications even in the background
+self.addEventListener('push', function(event) {
+    const data = event.data ? event.data.json() : {};
+    const title = data.title || "pH Monitor Alert";
+    const options = {
+        body: data.body || "Your pH levels are out of range!",
+        icon: 'https://cdn-icons-png.flaticon.com/512/822/822143.png',
+        badge: 'https://cdn-icons-png.flaticon.com/512/822/822143.png',
+        vibrate: [300, 100, 300],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '1'
+        }
+    };
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+// Forces the service worker to update immediately
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
 });
